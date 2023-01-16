f43a168cdbe4a1ecb685de4032b02a228e95a335
moniquetrotta <monique.trotta@gmail.com>
2023-01-16 15:49:43 -0300

    listagem completa de itens

---
+++ README.md
@@ -0,0 +1,21 @@
+#Aula Rocketseat - 5 truques incríveis com DOM que você deveria saber
+Base html e tailwind (https://www.fronteditor.dev/gists/d75d7978838126e93830ba97a71dd0e9)
+
+- O que é DOM?
+  Document Object Model
+- Visualizando a DOM
+
+---
+
+## Eventos
+
+- on + nomedoEvento
+- addEventListener ('nomedoEvento', function(event) {})
+
+## Criação de Elementos
+
+## .cloneNode()
+
+## classList
+
+## target
--- scripts.js
+++ scripts.js
@@ -9,9 +9,15 @@ form.onsubmit = function (event) {
 
   const li = document.createElement('li');
   li.setAttribute(
+    'class',
     'px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between',
   );
   li.textContent = value;
 
+  const button = document.createElement('button');
+  button.setAttribute = ('class', 'text-red-700 delete');
+  button.textContent = 'x';
+
+  li.appendChild(button);
   ul.appendChild(li);
 };
