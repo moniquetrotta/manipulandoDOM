008f168c03edc0bc96c7bdd8051b098af8211aa9
moniquetrotta <monique.trotta@gmail.com>
2023-01-16 16:09:07 -0300

    criando lista usando literals ``

--- scripts.js
+++ scripts.js
@@ -7,17 +7,7 @@ form.onsubmit = function (event) {
   const input = form.querySelector('input');
   const value = input.value;
 
-  const li = document.createElement('li');
-  li.setAttribute(
-    'class',
-    'px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between',
-  );
-  li.textContent = value;
+  ul.innerHTML += `<li class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out flex justify-between"><span>${value}</span><button class="text-red-700 delete">ⅹ</button></li>`;
 
-  const button = document.createElement('button');
-  button.setAttribute = ('class', 'text-red-700 delete');
-  button.textContent = 'x';
-
-  li.appendChild(button);
-  ul.appendChild(li);
+  input.value = '';
 };
